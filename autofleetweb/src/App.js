210d111027e06cc5dfb4f5e27d21d94bbf24e320
ChristySegunto <Christysgnt@gmail.com>
2024-10-30 04:27:38 +0800

    fixed route - not done

--- autofleetweb/src/App.js
+++ autofleetweb/src/App.js
@@ -3,6 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import Login from './Login/Login.js';
+import Sidebar from './components/Sidebar.js';
+
+
+import { AuthProvider } from './settings/AuthContext.js';
+import RequireAuth from './settings/RequireAuth.js';
+import ProtectedRoute from './settings/ProtectedRoute.js';
 
 import {
 	createBrowserRouter,
@@ -10,19 +16,33 @@ import {
 	Route,
 	RouterProvider,
 } from 'react-router-dom';
+import Dashboard from './admin/Dashboard.js';
 
 const router =createBrowserRouter(
   createRoutesFromElements(
     <Route path = "/"  >
       <Route index element={<Login />}></Route>
       <Route path = "login" element={<Login />}></Route>
+      <Route
+        path="dashboard"
+        element={
+          <RequireAuth>
+              <div className='web-container'>
+                <Sidebar/>
+                <Dashboard/>
+              </div>
+          </RequireAuth>
+        }
+      />
     </Route>
   )
 )
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   );
 }
 
