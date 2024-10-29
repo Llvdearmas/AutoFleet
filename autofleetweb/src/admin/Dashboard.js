478916c47a5b54cfa26715b1cb0988a5ce1f1e27
ChristySegunto <Christysgnt@gmail.com>
2024-10-30 04:27:48 +0800

    added dashboard page

---
+++ autofleetweb/src/admin/Dashboard.js
@@ -0,0 +1,12 @@
+
+import { Form, Button, Alert, Modal, Container } from 'react-bootstrap';
+
+function Dashboard() {
+    return (
+      <div className="Dashboard">
+        <Button variant="primary">This is Dashboard</Button>
+      </div>
+    );
+  }
+  
+  export default Dashboard;
